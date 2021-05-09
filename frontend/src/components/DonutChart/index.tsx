af317adad12c74b63c42c6d55b65da361a25f486
Luan Matos <luanmattos14@hotmail.com.br>
2021-05-09 16:06:28 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,23 +11,18 @@ type ChartData = {
 
 function DonutChart() {
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
-   
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-             
-            setChartData({ labels: myLabels, series: mySeries});
-        });    
-    } , []);
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     const options = {
         legend: {
